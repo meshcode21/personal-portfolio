0f3ab4bff8f117929d3d0a9c401b1d4f49d071cb
mahesh <maheshudas2077@gmail.com>
2025-05-26 08:43:09 +0545

    hero section completed

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ export default function App() {
     <>
     <Navbar/>
     <Hero/>
-    <div>
+    {/* <div className="bg-red-400 blur-2xl">ajhsefai</div> */}
+    <div className="w-full bg-gray-950">
+    <div className="text-xl container m-auto">
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos eveniet quia minima. Iure consequuntur accusantium hic at pariatur eius cum, ducimus, eveniet ipsum est in dolore suscipit minus beatae perspiciatis.
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur sapiente iusto odio velit necessitatibus unde nihil, maiores id soluta, sed omnis cumque aut eum delectus. Libero voluptatibus officia at iusto!
-    </div>
-    <div className="text-2xl font-saira">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, blanditiis tempora quo est facere quod libero quos eos exercitationem laborum accusamus, neque non ratione. At fugiat consectetur quasi explicabo cupiditate.
+    </div>
     </div>
     </>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,17 +2,20 @@ import heroPhoto from "../assets/mahesh.png";
 
 export default function Hero() {
   return (
-    <div className="container relative px-2 m-auto w-full h-[calc(100vh-80px)] flex flex-col justify-center">
+    <div className="container relative px-2 m-auto w-full h-[calc(100vh-80px)] max-h-[688px] flex flex-col justify-center overflow-x-visible">
       <div className="font-outfit font-semibold">
         <h1 className="text-4xl my-1">Hello Guys</h1>
-        <h1 className="text-5xl my-2">I'm Mahesh Udas</h1>
-        <h1 className="text-5xl">Full Stack Developer</h1>
+        <h1 className="text-6xl my-2">I'm Mahesh Udas</h1>
+        <h1 className="text-6xl">Full Stack Developer</h1>
       </div>
-      <div className="flex gap-2 w-1/2 h-40 my-3 items-center">
-        <div className="border w-25 h-25 flex justify-center items-center">X</div>
+      <div className="flex gap-5 w-1/2 h-40 my-3 items-center">
+        <div className="border w-25 h-25 flex justify-center items-center">
+          X
+        </div>
         <div className="flex gap-1 flex-col">
           <p className="">
-          Lorem ipsum dolo iandae, rem quae quae Lorem ipsum dolo iandae, rem quae
+            Lorem ipsum dolo iandae, rem quae quae Lorem ipsum dolo iandae, rem
+            quae
           </p>
           <a
             href="#"
@@ -22,15 +25,17 @@ export default function Hero() {
           </a>
         </div>
       </div>
-        {/* image */}
-        <div className="absolute right-0  w-[350px] h-[350px] rounded-full bg-[radial-gradient(circle,_#012f7b_0%,_#000d2b_60%)] z-0"></div>
 
-      <div className="absolute bottom-0 right-[15%]">
-        <img
-          src={heroPhoto}
-          alt="Mahesh Udas"
-          className="w-90"
-        />
+      {/* line  */}
+      <div className="-z-10 absolute -top-36 -right-58 rotate-[30deg] w-7/12 h-[300px] blur-[100px] bg-red-600/50" />
+      {/* line circle  */}
+      <div className="-z-10 absolute -bottom-8 -right-40 w-[550px] h-[550px] border border-gray-500/50 rounded-[50%]" />
+      <div className="-z-10 absolute -bottom-10 -right-20 w-[450px] h-[450px] border border-gray-500/50 rounded-[50%]" />
+      {/* circle */}
+      <div className="-z-10 absolute bottom-0 right-[13%] translate-x-1/4  w-[500px] h-[500px] rounded-full bg-blue-600/70 blur-[100px]"></div>
+      {/* image */}
+      <div className="absolute right-[15%] translate-x-1/6 bottom-0 bg-opacity-0">
+        <img src={heroPhoto} alt="Mahesh Udas" className="w-90 relative" />
       </div>
     </div>
   );
